42a36e9cbfd7e605ab277c5780161b73bb227162
hoangha1908 <hoangha19081988@gmail.com>
2020-01-09 14:52:00 +0700

    Update example.tf

--- example.tf
+++ example.tf
@@ -1,14 +1,17 @@
-provider "aws" {
-  profile    = "default"
-  region     = "us-east-1"
+resource "aws_s3_bucket" "example" {
+  # NOTE: S3 bucket names must be unique across _all_ AWS accounts, so
+  # this name must be changed before applying this example to avoid naming
+  # conflicts.
+  bucket = "hoangha1908"
+  acl    = "private"
 }
 
+# Change the aws_instance we declared earlier to now include "depends_on"
 resource "aws_instance" "example" {
   ami           = "ami-0c322300a1dd5dc79"
-  instance_type = "t1.micro"
-  key_name = "hoangha2"
-}
-resource "aws_eip" "ip" {
-    vpc = true
-    instance = aws_instance.example.id
+  instance_type = "t2.micro"
+
+  # Tells Terraform that this EC2 instance must be created only after the
+  # S3 bucket has been created.
+  depends_on = [aws_s3_bucket.example]
 }
