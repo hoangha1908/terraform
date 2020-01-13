fdec7f986747850ecd78caab1822612124355b1a
hoangha1908 <hoangha19081988@gmail.com>
2020-01-13 20:48:13 +0700

    abc

--- example.tf
+++ example.tf
@@ -12,6 +12,28 @@ resource "aws_instance" "example" {
 }
 
 
+resource "aws_key_pair" "example" {
+  key_name = "hoangha2"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
 
+resource "aws_instance" "web" {
+  key_name = aws_key_pair.example.hoangha2
+  # ...
+
+ connection {
+    type     = "ssh"
+    user     = "root"
+    private_key = file("~/.ssh/id_rsa")
+    host     = self.ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo amazon-linux-extras enable nginx1.12",
+      "sudo yum -y install nginx",
+      "sudo systemctl start nginx"
+    ]
+  }
 
 
