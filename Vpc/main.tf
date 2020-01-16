7271d4d0030dac53f669ef9c5ed04aaed8595b12
hoangha1908 <hoangha19081988@gmail.com>
2020-01-16 13:40:15 +0700

    1

--- Vpc/main.tf
+++ Vpc/main.tf
@@ -60,7 +60,7 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_route_table" "route_table_public" {
   vpc_id=aws_vpc.vpc.id
   route {
-      cidr_block = "0.0.0.0/24"
+      cidr_block = "0.0.0.0/0"
       gateway_id = aws_internet_gateway.igw.id
   }
   tags = {
@@ -70,10 +70,7 @@ resource "aws_route_table" "route_table_public" {
 }
 resource "aws_route_table" "route_table_private" {
   vpc_id=aws_vpc.vpc.id
-  route {
-      cidr_block = "0.0.0.0/24"   
-  }
-  tags = {
+   tags = {
       Name = "${var.project}-route-private-${var.environment}"
       Environment = var.environment
   }
