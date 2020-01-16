f97fbed9a8d70da13c5e9efc92eb86c83298a273
hoangha1908 <hoangha19081988@gmail.com>
2020-01-16 10:06:26 +0700

    1

--- Vpc/main.tf
+++ Vpc/main.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "vpc" {
+resource "aws_vpc" "${var.project}-vpc-${var.environment}" {
   cidr_block = var.cidr_block
   enable_dns_support = true
   enable_dns_hostnames = true
@@ -9,3 +9,4 @@ resource "aws_vpc" "vpc" {
          Environment = var.environment
        }
 }
+
