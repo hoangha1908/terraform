fbaf90addf0b232c95aad83d484c0cc8350f882a
hoangha1908 <hoangha19081988@gmail.com>
2020-01-16 10:15:32 +0700

    1

--- Vpc/main.tf
+++ Vpc/main.tf
@@ -1,12 +1,21 @@
-resource "aws_vpc" "$${var.project}-vpc-$${var.environment}" {
+resource "aws_vpc" "vpc" {
   cidr_block = var.cidr_block
   enable_dns_support = true
   enable_dns_hostnames = true
-  enable_classiclink = false
   instance_tenancy = "default"
   tags = {
          Name = "${var.project}-vpc-${var.environment}"
          Environment = var.environment
        }
+}
+resource "aws_subnet" "subnet_public" {
+  vpc_id = aws_vpc.vpc.id 
+  cidr_block ="10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+      Name = "${var.project}-subnet_public-${var.environment}"
+      Environment = var.environment
+  }
 }
 
